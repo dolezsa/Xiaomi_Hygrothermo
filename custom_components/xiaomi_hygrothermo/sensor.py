9f1ca3f41dc0ab707f06d68078b046d57a714e6f
Kendell R <KTibow@users.noreply.github.com>
2020-09-25 17:23:58 -0700

    Format + GitHub Actions

--- custom_components/xiaomi_hygrothermo/sensor.py
+++ custom_components/xiaomi_hygrothermo/sensor.py
@@ -1,36 +1,43 @@
-import re
 import logging
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+import re
 from datetime import timedelta
-from homeassistant.const import (ATTR_BATTERY_LEVEL, CONF_NAME, CONF_MAC, CONF_SCAN_INTERVAL, TEMP_CELSIUS)
-from homeassistant.helpers.entity import Entity
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    ATTR_BATTERY_LEVEL,
+    CONF_MAC,
+    CONF_NAME,
+    CONF_SCAN_INTERVAL,
+    TEMP_CELSIUS,
+)
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import track_time_interval
 from homeassistant.util.dt import utcnow
 
-REQUIREMENTS = ['bluepy==1.1.4']
+REQUIREMENTS = ["bluepy==1.1.4"]
 
 _LOGGER = logging.getLogger(__name__)
 
-BT_MAC = vol.All(
-    cv.string,
-    vol.Length(min=17, max=17)
-)
+BT_MAC = vol.All(cv.string, vol.Length(min=17, max=17))
 
 SCAN_INTERVAL = timedelta(seconds=30)
 NAME = "Mijia BT Hygrothermograph"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_MAC, default=None): vol.Any(BT_MAC, None),
-    vol.Optional(CONF_NAME, default=NAME): cv.string,
-    vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_MAC, default=None): vol.Any(BT_MAC, None),
+        vol.Optional(CONF_NAME, default=NAME): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL, default=SCAN_INTERVAL): cv.time_period,
+    }
+)
 
 SENSOR_TYPES = {
-    'Temperature': [TEMP_CELSIUS, 'mdi:thermometer'],
-    'Humidity': ['%', 'mdi:water-percent']
+    "Temperature": [TEMP_CELSIUS, "mdi:thermometer"],
+    "Humidity": ["%", "mdi:water-percent"],
 }
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
@@ -38,6 +45,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     add_devices(device.entities)
 
     track_time_interval(hass, device.get_data, config.get(CONF_SCAN_INTERVAL))
+
 
 class XiomiHygroThermoDelegate(object):
     def __init__(self):
@@ -47,10 +55,11 @@ class XiomiHygroThermoDelegate(object):
 
     def handleNotification(self, cHandle, data):
         if cHandle == 14:
-            m = re.search('T=([\d\.]*)\s+?H=([\d\.]*)', ''.join(map(chr, data)))
+            m = re.search("T=([\d\.]*)\s+?H=([\d\.]*)", "".join(map(chr, data)))
             self.temperature = m.group(1)
             self.humidity = m.group(2)
             self.received = True
+
 
 class XiomiHygroThermo(object):
     def __init__(self, hass, name, address):
@@ -61,26 +70,29 @@ class XiomiHygroThermo(object):
         self.last_battery = None
 
         self.entities = [
-            XiomiHygroThermoEntity(hass, name, 'Temperature'),
-            XiomiHygroThermoEntity(hass, name, 'Humidity')
+            XiomiHygroThermoEntity(hass, name, "Temperature"),
+            XiomiHygroThermoEntity(hass, name, "Humidity"),
         ]
 
         self.get_data()
 
-    def get_data(self, now = None):
+    def get_data(self, now=None):
         try:
             from bluepy import btle
 
             p = btle.Peripheral(self.address)
 
-            #self.name = ''.join(map(chr, p.readCharacteristic(0x3)))
-            #self.firmware = ''.join(map(chr, p.readCharacteristic(0x24)))
-            if self.last_battery is None or (utcnow() - self.last_battery).seconds >= 3600:
+            # self.name = ''.join(map(chr, p.readCharacteristic(0x3)))
+            # self.firmware = ''.join(map(chr, p.readCharacteristic(0x24)))
+            if (
+                self.last_battery is None
+                or (utcnow() - self.last_battery).seconds >= 3600
+            ):
                 self.battery = p.readCharacteristic(0x18)[0]
                 self.last_battery = utcnow()
 
             delegate = XiomiHygroThermoDelegate()
-            p.withDelegate( delegate )
+            p.withDelegate(delegate)
             p.writeCharacteristic(0x10, bytearray([1, 0]), True)
             while not delegate.received:
                 p.waitForNotifications(30.0)
@@ -97,14 +109,17 @@ class XiomiHygroThermo(object):
             ok = False
 
         for i in [0, 1]:
-            changed = self.entities[i].set_state(ok, self.battery, self.temperature if i == 0 else self.humidity)
+            changed = self.entities[i].set_state(
+                ok, self.battery, self.temperature if i == 0 else self.humidity
+            )
             if (not now is None) and changed:
                 self.entities[i].async_schedule_update_ha_state()
+
 
 class XiomiHygroThermoEntity(Entity):
     def __init__(self, hass, name, device_type):
         self.hass = hass
-        self._name = '{} {}'.format(name, device_type)
+        self._name = "{} {}".format(name, device_type)
         self._state = None
         self._is_available = True
         self._type = device_type
@@ -170,5 +185,5 @@ class XiomiHygroThermoEntity(Entity):
         if self.__errcnt > 3:
             self._is_available = False
             changed = True
-        
+
         return changed
